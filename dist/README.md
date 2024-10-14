135d95b8eb00a89570158f6fc27aababbea0eb90
Egor Im <egorim@phys.ethz.ch>
2024-10-14 17:50:03 +0200

    for release 0.9.0: aux files/edited

---
+++ dist/README.md
@@ -0,0 +1,13 @@
+# Article Management Tool v0.0.9
+
+Pre-release version includes:
+- **Executable for Linux**: `article-management-tool`
+- **This instructions**: `README.md`
+
+## Instructions
+
+1. Download the executable.
+2. Make the file executable
+```bash
+    chmod +x article-management-tool
+```
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+thisDirectory = Path(__file__).parent
+longDescription = (thisDirectory / "README.md").read_text()
+
+setup(
+    name="article-management-tool",
+    version="0.0.9",
+    author="Egor Im",
+    author_email="egor.im.97@gmail.com",
+    description="Manage articles, books, lecture notes, etc for research.",
+    long_description=longDescription,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eg0rim/amt",
+    packages=find_packages(where="amt"),
+    package_dir={"": "amt"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12.3",
+    install_requires=[
+        "PySide6>=6.7.3",
+    ],
+    entry_points={
+        "console_scripts": [
+            "article-management-tool=amt.main:main",
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
